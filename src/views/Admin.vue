b1d3d964eaa68bb2c2fdc11cf9d33ecef84aee15
guylil <guylilian@gmail.com>
2021-10-17 13:49:23 +0300

    admin palne ready

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="admin">
-    <h1>This is an admin page</h1>
-  </div>
+  <v-container>
+    <div class="admin">
+      <h1>Phishing test statuses</h1>
+    </div>
+    <div></div>
+    <v-data-table
+        :headers="headers"
+        :items="items"
+        :items-per-page="20"
+        class="elevation-1 mt-6"
+        :loading="items.length===0"
+    ></v-data-table>
+  </v-container>
+
 </template>
+<script>
+export default {
+  name: 'Admin',
+  data:()=> ({
+    headers: [
+        {text: 'Email', align: 'start', sortable: true, value:'email'},
+        {text: 'Status',  value:'status'}
+      ],
+    items:[]
+
+  }),
+  mounted() {
+    this.items = this.getData()
+  },
+  methods:{
+    getData(){
+      return [
+        {email: 'a@cy.com', status:'opened'},
+        {email: 'b@cy.com', status:'received'}
+      ]
+    }
+  }
+
+}
+</script>
